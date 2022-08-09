0a429a405269b7a5cb3ab700a0f8220e8dcdddea
manoj khatri <61318643+khatrimanoj@users.noreply.github.com>
2022-08-09 18:15:26 +0530

    manoj chnges

--- src/components/ProductTicker/ProductTicker.js
+++ src/components/ProductTicker/ProductTicker.js
@@ -1,21 +1,21 @@
-import React, { useState, useEffect } from "react";
+import React, {useState, useEffect} from 'react';
 
 const ProductTicker = (props) => {
   const [csvData, setCsvData] = useState([]);
 
-  useEffect(() => {
-    const fetchCsv = async () => {
-      return await fetch(
-        "https://s3.ap-southeast-1.amazonaws.com/eiq.ai.tempcsv/Eiqai.ticker_new.csv"
-      )
-        .then((response) => {
-          return response.text();
-        })
-        .then((res) => {
-          data(res);
-        });
+  const fetchCsv = async () => {
+    return await fetch(
+      "https://s3.ap-southeast-1.amazonaws.com/eiq.ai.tempcsv/Eiqai.ticker_new.csv"
+    )
+      .then((response) => {
+        return response.text();
+      })
+      .then((res) => {
+        data(res);
+      });
     };
-
+    
+  useEffect(() => {
     fetchCsv();
   }, []);
 
@@ -35,38 +35,39 @@ const ProductTicker = (props) => {
     setCsvData(d1);
   };
 
-  const LoopList = () => {
-    return (
-      <>
-        {csvData.map((e, i) => (
-          <>
-            {e.name !== "" &&
-              e.name !== "undefined" &&
-              e.value !== "" &&
-              e.value !== "undefined" && (
-                <span
-                  key={i}
-                  className="inline-flex items-center text-1xl mx-4"
-                >
-                  <span className="text-[clamp(0.813rem,1.5vw,0.935rem)] text-white pr-10px">
-                    {e.name}
-                  </span>
-                  <div className="status-text flex items-center">
-                    <span className={"status color-" + e.color}></span>
-                    <span className="text-[clamp(0.813rem,1.5vw,0.935rem)] text-white">
-                      {e.value}
+   
+    const LoopList = () => {
+      return (
+        <>
+          {csvData.map((e, i) => (
+            <>
+              {e.name !== "" &&
+                e.name !== "undefined" &&
+                e.value !== "" &&
+                e.value !== "undefined" && (
+                  <span
+                    key={i}
+                    className="inline-flex items-center text-1xl mx-4"
+                  >
+                    <span className="text-[clamp(0.813rem,1.5vw,0.935rem)] text-white pr-10px">
+                      {e.name}
                     </span>
-                  </div>
-                </span>
-              )}
-          </>
-        ))}
-      </>
-    );
-  };
+                    <div className="status-text flex items-center">
+                      <span className={"status color-" + e.color}></span>
+                      <span className="text-[clamp(0.813rem,1.5vw,0.935rem)] text-white">
+                        {e.value}
+                      </span>
+                    </div>
+                  </span>
+                )}
+            </>
+          ))}
+        </>
+      );
+    };
 
   return (
-    <div className="absolute w-full">
+    <div className='absolute w-full'>
       <div className="marquee mx-auto w-full border-t-0 xl:border-t-1 relative flex overflow-x-hidden">
         <div className="py-2 animate-marquee whitespace-nowrap">
           <LoopList />
@@ -77,6 +78,6 @@ const ProductTicker = (props) => {
       </div>
     </div>
   );
-};
+}
 
 export default ProductTicker;
