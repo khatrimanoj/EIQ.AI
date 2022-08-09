7db2481714adec885d7d689c6a78f8ab57af3649
manoj khatri <61318643+khatrimanoj@users.noreply.github.com>
2022-08-09 18:05:33 +0530

    manoj chnges

--- src/components/ProductTicker/ProductTicker.js
+++ src/components/ProductTicker/ProductTicker.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, { useState, useEffect } from "react";
 
 const ProductTicker = (props) => {
   const [csvData, setCsvData] = useState([]);
@@ -14,9 +14,10 @@ const ProductTicker = (props) => {
         data(res);
       });
   };
-  // useEffect(() => {
-  // }, []);
-  fetchCsv();
+
+  useEffect(() => {
+    fetchCsv();
+  }, []);
 
   const data = (res) => {
     let data = res.split(/\r?\n/);
@@ -34,39 +35,38 @@ const ProductTicker = (props) => {
     setCsvData(d1);
   };
 
-   
-    const LoopList = () => {
-      return (
-        <>
-          {csvData.map((e, i) => (
-            <>
-              {e.name !== "" &&
-                e.name !== "undefined" &&
-                e.value !== "" &&
-                e.value !== "undefined" && (
-                  <span
-                    key={i}
-                    className="inline-flex items-center text-1xl mx-4"
-                  >
-                    <span className="text-[clamp(0.813rem,1.5vw,0.935rem)] text-white pr-10px">
-                      {e.name}
-                    </span>
-                    <div className="status-text flex items-center">
-                      <span className={"status color-" + e.color}></span>
-                      <span className="text-[clamp(0.813rem,1.5vw,0.935rem)] text-white">
-                        {e.value}
-                      </span>
-                    </div>
+  const LoopList = () => {
+    return (
+      <>
+        {csvData.map((e, i) => (
+          <>
+            {e.name !== "" &&
+              e.name !== "undefined" &&
+              e.value !== "" &&
+              e.value !== "undefined" && (
+                <span
+                  key={i}
+                  className="inline-flex items-center text-1xl mx-4"
+                >
+                  <span className="text-[clamp(0.813rem,1.5vw,0.935rem)] text-white pr-10px">
+                    {e.name}
                   </span>
-                )}
-            </>
-          ))}
-        </>
-      );
-    };
+                  <div className="status-text flex items-center">
+                    <span className={"status color-" + e.color}></span>
+                    <span className="text-[clamp(0.813rem,1.5vw,0.935rem)] text-white">
+                      {e.value}
+                    </span>
+                  </div>
+                </span>
+              )}
+          </>
+        ))}
+      </>
+    );
+  };
 
   return (
-    <div className='absolute w-full'>
+    <div className="absolute w-full">
       <div className="marquee mx-auto w-full border-t-0 xl:border-t-1 relative flex overflow-x-hidden">
         <div className="py-2 animate-marquee whitespace-nowrap">
           <LoopList />
@@ -77,6 +77,6 @@ const ProductTicker = (props) => {
       </div>
     </div>
   );
-}
+};
 
 export default ProductTicker;
